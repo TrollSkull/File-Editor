20a0912a8f5917153efb5089099998b62622e83d
TrollSkull <64570084+TrollSkull@users.noreply.github.com>
2020-11-15 03:58:20 +0100

    Update file-editor.py

--- file-editor.py
+++ file-editor.py
@@ -37,7 +37,8 @@ while (loop == 1):
   ---------------	-------------------------------------
    editor		 Edit files
    analyzer		 Parse any kind of file
-  
+   
+   ls			 Muestra los archivos cercanos
    change log		 The list of changes in this version
    about		 Show information about this program
    clear		 Clean the terminal
@@ -54,12 +55,22 @@ Change in program structure""")
 		print(chr(27)+"[1;37m"+"""
 Improvement when editing
 
+Bug fixes""")
+		print(chr(27)+"[1;33m"+"\nChanges 1.3:")
+		print(chr(27)+"[1;37m"+"""
+Added \"ls\" command
+
+Bug fixes""")
+		print(chr(27)+"[1;33m"+"\nChanges 1.4:")
+		print(chr(27)+"[1;37m"+"""
+Fixed the \"ls\" command error in Windows
+
 Bug fixes
 """)
 	elif ter == "about":
 		print(chr(27)+"[1;33m"+"""
    Author:"""+chr(27)+"[1;37m"+""" 	     	 TrollSkull
-   """+chr(27)+"[1;33m"+"""Version:"""+chr(27)+"[1;37m"+"""   	 	 Alpha 1.2"""+chr(27)+"[1;33m"+"""
+   """+chr(27)+"[1;33m"+"""Version:"""+chr(27)+"[1;37m"+"""   	 	 Alpha 1.4"""+chr(27)+"[1;33m"+"""
    Name:"""+chr(27)+"[1;37m"+"""		 Python File Editor"""+chr(27)+"[1;33m"+"""
    Contact:"""+chr(27)+"[1;37m"+"""		 trollskull.contact@gmail.com
    
@@ -87,13 +98,25 @@ Bug fixes
 		print(chr(27)+"[1;34m"+"\n["+chr(27)+"[1;32m"+"✓"+chr(27)+"[1;34m"+"]"+chr(27)+"[1;37m"+" Editor activated\n")
 		print(chr(27)+"[1;30m"+"[INFO] Type the file to edit or you can type\nthe name of a non-existent file to create it\n")
 		edit = input(chr(27)+"[1;0;37m"+"File/Editor "+chr(27)+"[1;33m"+">>"+chr(27)+"[1;37m"+" ")
-		try:
-			print(chr(27)+"[1;30m"+"\n[INFO] Type the new content\n")
-			edited = input(chr(27)+"[1;0;37m"+"File/Editor/Text "+chr(27)+"[1;33m"+">>"+chr(27)+"[1;37m"+" ")
-			with open(edit, "w") as f:
-				f.write(edited)
-			print(chr(27)+"[1;30m"+"\n[INFO] Successful file edited\n")
-		except:
-				print("")
+		if edit == "file-editor.py":
+			print(chr(27)+"[1;30m"+"\n[INFO] No se puede editar este archivo\n")
+		else:
+			try:
+				print(chr(27)+"[1;30m"+"\n[INFO] Type the new content\n")
+				edited = input(chr(27)+"[1;0;37m"+"File/Editor/Text "+chr(27)+"[1;33m"+">>"+chr(27)+"[1;37m"+" ")
+				with open(edit, "w") as f:
+					f.write(edited)
+				print(chr(27)+"[1;30m"+"\n[INFO] Successful file edited\n")
+			except:
+					print("")
+	elif ter == "ls":
+		if sys.platform == "win32":
+			print(chr(27)+"[1;30m"+"\n[INFO] Archivos cercanos\n"+chr(27)+"[1;33m")
+			os.system("dir /w")
+			print("")
+		else:
+			print(chr(27)+"[1;30m"+"\n[INFO] Archivos cercanos\n"+chr(27)+"[1;33m")
+			os.system("ls")
+			print("")
 	else:
 		print(chr(27)+"[1;34m"+"\n["+chr(27)+"[1;31m"+"!"+chr(27)+"[1;34m"+"]"+chr(27)+"[1;37m"+" Command \"" + ter + "\" not found\n")
